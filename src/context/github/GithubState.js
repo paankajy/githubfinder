31b86c349bd6af94d49d116551c09aa1b3f3e0d7
pankaj <paankajy@gmail.com>
2020-06-22 11:21:51 +0530

    final

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -22,14 +22,49 @@ const GithubState = (props) => {
   const [state, dispatch] = useReducer(GithubReducer, initialState);
 
   // Search users
+  const searchUsers = async (text) => {
+    setLoading();
 
-  //Get users
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=
+    ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    dispatch({
+      type: SEARCH_USERS,
+      payload: res.data.items,
+    });
+  };
+
+  //Get user
+  const getUser = async (username) => {
+    setLoading();
+
+    const res = await axios.get(`https://api.github.com/users/${username}?&client_id=
+    ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    dispatch({
+      type: GET_USER,
+      payload: res.data,
+    });
+  };
 
   //Get Repos
 
-  //Clear users
+  const getUserRepos = async (username) => {
+    setLoading();
+    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=
+     ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    dispatch({
+      type: GET_REPOS,
+      payload: res.data,
+    });
+  };
+
+  // Clear user from state
+  const clearUsers = () => dispatch({ type: CLEAR_USERS });
 
   //Set loading
+  const setLoading = () => dispatch({ type: SET_LOADING });
 
   return (
     <GithubContext.Provider
@@ -38,6 +73,10 @@ const GithubState = (props) => {
         user: state.user,
         repos: state.repos,
         loading: state.loading,
+        searchUsers,
+        clearUsers,
+        getUser,
+        getUserRepos,
       }}
     >
       {props.children}
